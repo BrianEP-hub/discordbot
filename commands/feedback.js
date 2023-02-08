2b2715b786b3a95bf45a243565c4d5982e6c1989
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2023-02-08 15:36:53 +0100

    added project command

---
+++ commands/feedback.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('feedback')
+		.setDescription('Request feedback')
+        .addStringOption(option => 
+            option
+                .setName('project')
+                .setDescription('Project')
+            ),
+	async execute(interaction) {
+        const project = interaction.options.getString('project')
+		return interaction.reply(
+            `<@${interaction.user.id}> is looking for feedback on his ${project}`
+            );
+	},
+};
